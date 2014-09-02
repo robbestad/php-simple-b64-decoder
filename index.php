add0e8f6b5210c5888f94dfcd89263d632c870b1
Sven Anders Robbestad <robbestad@gmail.com>
2014-09-02 08:51:03 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$id=$_GET["id"];
+$image=explode(".",$id);
+$base=file_get_contents("https://metagram.firebaseio.com/grams/".$image[0]."/base64.json");
+$imgstr=json_decode($base);
+$new_data=explode(";",$imgstr);
+if(count($new_data)>1){
+	$type=$new_data[0];
+	$data=explode(",",$new_data[1]);
+	header("Content-type:".$type);
+	echo base64_decode($data[1]);
+}
+else{
+ 	$type="image"; // just a guess
+	header("Content-type:".$type);
+	echo base64_decode($imgstr);
+}
